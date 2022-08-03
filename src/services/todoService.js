100ce4e4970e6b1412b1ad23a5700dd550063ae8
Yussif <djiceman@gmail.com>
2022-08-03 13:24:23 +0000

    update

--- src/services/todoService.js
+++ src/services/todoService.js
@@ -1,5 +1,6 @@
 const baseUrl = `${process.env.REACT_APP_API_URL}/todo`;
 
+
 export const loadTodos = () => {
     return fetch(baseUrl).then((res) => res.json());
 }
@@ -10,24 +11,24 @@ export const getTodo = (id) => {
 
 export const createTodo = (todo) => {
     return fetch(baseUrl, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            title: todo.title,
-            completed: todo.completed
-        }),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: todo.title,
+        completed: todo.completed,
+      }),
     }).then((res) => res.json());
 }
 
 export const updateTodo = (todo) => {
     return fetch(`${baseUrl}/${todo.id}`, {
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            id: todo.id,
-            title: todo.title,
-            completed: todo.completed
-        }),
+      method: "PUT",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        id: todo.id,
+        title: todo.title,
+        completed: todo.completed,
+      }),
     }).then((res) => res.json());
 }
 
